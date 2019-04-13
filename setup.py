d30a15febba6189d74ccecc0e6e40384ef7eff05
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-04-13 16:26:45 +0300

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
-
-version = open('VERSION').read().strip()
-license = open('LICENSE').read().strip()
 
 setup(
     name='dictionary-profanity-filter',
-    version=version,
-    license=license,
+    version=open('VERSION').read().strip(),
+    license=open('LICENSE').read().strip(),
     author='Dmitry Shorokhov',
     author_email='vip.shoroch@gmail.com',
     description='Python profanity filter',
     long_description=open('README.md').read().strip(),
     packages=find_packages(),
-    install_requires=[
-        parse_requirements('requirements.txt')
-    ],
+    install_requires=open('requirements.txt').read().splitlines(),
     test_suite='tests',
     entry_points={
         'console_scripts': [
