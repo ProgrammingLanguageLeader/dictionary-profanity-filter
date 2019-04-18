6af31cb8b2bae15f4bed691e9b2739908a1bea6b
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-04-19 00:18:41 +0300

    Refactoring & using word cases

--- dictionary_profanity_filter/profanity_filter.py
+++ dictionary_profanity_filter/profanity_filter.py
@@ -22,19 +22,21 @@ class ProfanityFilter:
             custom_censor_list: Sequence[str] = None,
             extra_censor_list: Sequence[str] = None,
             no_word_boundaries: bool = False,
-            censor_char: str = '*'
+            censor_char: str = '*',
+            use_word_cases: bool = False,
     ) -> None:
-        self._censor_list = list(custom_censor_list or [])
-        self._extra_censor_list = list(extra_censor_list or [])
-        self._no_word_boundaries = no_word_boundaries
-        self._complete_censor_list = []
-        self._censor_char = censor_char
-        self._morph_analyzer = pymorphy2.MorphAnalyzer()
+        self.censor_list = list(custom_censor_list or [])
+        self.extra_censor_list = list(extra_censor_list or [])
+        self.no_word_boundaries = no_word_boundaries
+        self.use_word_cases = use_word_cases
+        self.complete_censor_list = []
+        self.censor_char = censor_char
+        self.morph_analyzer = pymorphy2.MorphAnalyzer()
         if not custom_censor_list:
-            self._load_words()
-        self._complete_censor_list = self._calc_complete_censor_list()
+            self.load_words()
+        self.complete_censor_list = self.calc_complete_censor_list()
 
-    def _load_words(self) -> None:
+    def load_words(self) -> None:
         base_dir = os.path.abspath(os.path.dirname(__file__))
         words_file_path = os.path.join(
             base_dir,
@@ -42,19 +44,19 @@ class ProfanityFilter:
             'default_list.txt'
         )
         with open(words_file_path, 'r') as words_file:
-            self._censor_list = [
+            self.censor_list = [
                 line.strip() for line in words_file.readlines()
             ]
 
-    def _calc_complete_censor_list(self) -> List[str]:
+    def calc_complete_censor_list(self) -> List[str]:
         complete_censor_list = []
-        complete_censor_list.extend(self._censor_list)
-        complete_censor_list.extend(self._extra_censor_list)
-        pluralized_words = self._pluralize_words(complete_censor_list)
+        complete_censor_list.extend(self.censor_list)
+        complete_censor_list.extend(self.extra_censor_list)
+        pluralized_words = self.get_words_cases(complete_censor_list)
         complete_censor_list.extend(pluralized_words)
         return list(set(complete_censor_list))
 
-    def _pluralize_words(self, words: Sequence[str]) -> List[str]:
+    def get_words_cases(self, words: Sequence[str]) -> List[str]:
         pluralized_words = []
         alphabet_detector = AlphabetDetector()
         if isinstance(words, str):
@@ -64,10 +66,7 @@ class ProfanityFilter:
             if 'LATIN' in alphabets:
                 pluralized_words.append(inflection.pluralize(word))
             elif 'CYRILLIC' in alphabets:
-                parsed_word = self._morph_analyzer.parse(word)[0]
-                inflected_word = parsed_word.inflect({'plur'})
-                if inflected_word:
-                    pluralized_words.append(inflected_word.word)
+                pluralized_words += self.get_cyrillic_word_cases(word)
             else:
                 self.logger.warn(
                     'Unsupported language for text: {}'.format(
@@ -76,49 +75,53 @@ class ProfanityFilter:
                 )
         return pluralized_words
 
-    def set_censor_char(self, character: str) -> None:
-        self._censor_char = character
-
-    def get_censor_list(self) -> List[str]:
-        return self._censor_list
-
-    def get_extra_censor_list(self) -> List[str]:
-        return self._extra_censor_list
-
-    def get_complete_censor_list(self) -> List[str]:
-        return self._complete_censor_list
+    def get_cyrillic_word_cases(self, word: str) -> List[str]:
+        cyrillic_word_cases = []
+        parsed_word = self.morph_analyzer.parse(word)[0]
+        if self.use_word_cases:
+            cyrillic_word_cases += [
+                word_form.word for word_form in parsed_word.lexeme
+            ]
+        parsed_word = parsed_word.inflect({'plur'})
+        if self.use_word_cases and parsed_word:
+            cyrillic_word_cases += [
+                word_form.word for word_form in parsed_word.lexeme
+            ]
+        elif parsed_word:
+            cyrillic_word_cases.append(parsed_word.word)
+        return cyrillic_word_cases
 
     def add_words(self, words: Sequence[str]) -> None:
         if isinstance(words, str):
             words = [words]
         words = list(words)
-        self._extra_censor_list += words
-        self._complete_censor_list += words
-        self._complete_censor_list += self._pluralize_words(words)
-        self._complete_censor_list = list(set(self._complete_censor_list))
+        self.extra_censor_list += words
+        self.complete_censor_list += words
+        self.complete_censor_list += self.get_words_cases(words)
+        self.complete_censor_list = list(set(self.complete_censor_list))
 
     def remove_word(self, word: str) -> None:
-        self._extra_censor_list.remove(word)
-        pluralized_words = self._pluralize_words(word)
+        self.extra_censor_list.remove(word)
+        pluralized_words = self.get_words_cases(word)
         for pluralized_word in pluralized_words:
-            self._complete_censor_list.remove(pluralized_word)
-        self._complete_censor_list.remove(word)
+            self.complete_censor_list.remove(pluralized_word)
+        self.complete_censor_list.remove(word)
 
     def censor(self, input_text: str) -> str:
         censored_text = input_text
-        for word in self._complete_censor_list:
-            regex_string = r'{0}' if self._no_word_boundaries else r'\b{0}\b'
+        for word in self.complete_censor_list:
+            regex_string = r'{0}' if self.no_word_boundaries else r'\b{0}\b'
             regex_string = regex_string.format(word)
             regex = re.compile(regex_string, re.IGNORECASE)
             censored_text = regex.sub(
-                self._censor_char * len(word),
+                self.censor_char * len(word),
                 censored_text
             )
         return censored_text
 
     def is_clean(self, input_text: str) -> bool:
-        for word in self._complete_censor_list:
-            regex_string = r'{0}' if self._no_word_boundaries else r'\b{0}\b'
+        for word in self.complete_censor_list:
+            regex_string = r'{0}' if self.no_word_boundaries else r'\b{0}\b'
             regex_string = regex_string.format(word)
             regex = re.compile(regex_string, re.IGNORECASE)
             if regex.search(input_text):
